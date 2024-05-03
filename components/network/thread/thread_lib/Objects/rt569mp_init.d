06e5d5e1eecd3d03a6044e08278e65afec5492f7
jiemin.cao@rafaelmicro.com <jiemin.cao@rafaelmicro.com>
2024-05-03 14:50:46 +0800

    del src

--- components/network/thread/thread_lib/Objects/rt569mp_init.d
+++
@@ -1,2 +0,0 @@
-./objects/rt569mp_init.o: ..\..\rt569mp\Src\rt569mp_init.c \
-  ..\..\rt569_rf\include\rf_mcu_chip.h
--- components/network/thread/thread_lib/Objects/subg_ctrl.d
+++
@@ -1,7 +0,0 @@
-./objects/subg_ctrl.o: ..\..\subg_ctrl\subg_ctrl.c \
-  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdio.h \
-  ..\..\subg_ctrl\Inc\subg_ctrl.h \
-  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdint.h \
-  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdlib.h \
-  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\string.h \
-  ..\..\..\platform\hosal\rt582_hosal\Inc\hosal_rf.h
